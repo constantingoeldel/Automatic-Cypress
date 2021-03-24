201e90c35e642ec59f324a3d1f6051949b67c7b4
Constantin <constantin_goeldel@t-online.de>
2021-03-24 19:15:11 +0300

    set es modules

--- cypress.js
+++ cypress.js
@@ -1,10 +1,12 @@
-const { exec } = require('child_process')
+// const { exec } = require('child_process')
+import { exec } from 'child_process'
 
 export default function cypress(url, app) {
   console.log(`Cypress test started for ${app} app on url ${url}`)
+  console.log(`URL=${url} npm run ${app}`)
   const testResult = exec(`URL=${url} npm run ${app}`, (error, stdout, stderr) => {
     if (error) {
-      console.log(`error: ${error.message}`)
+      console.log(`error: ${error}`)
       return error
     }
     if (stderr) {
